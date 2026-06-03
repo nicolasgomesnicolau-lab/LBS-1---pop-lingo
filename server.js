490ede75de7bd4cf6189ef77fd839ec2874e527f
zonestore44552-web <zonestore44552@gmail.com>
2026-06-03 14:39:51 -0300

    fix: use literal CORS origin instead of user input

--- server.js
+++ server.js
@@ -185,12 +185,7 @@ http.createServer((req, res) => {
   const url = new URL(req.url, 'http://localhost:' + PORT);
   const method = req.method;
 
-  const origin = req.headers.origin || '';
-  if (origin && (origin.startsWith('http://localhost') || origin === 'https://lbs-1-pop-lingo.onrender.com')) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  } else if (!origin) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://lbs-1-pop-lingo.onrender.com');
-  }
+  res.setHeader('Access-Control-Allow-Origin', 'https://lbs-1-pop-lingo.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('X-Content-Type-Options', 'nosniff');
